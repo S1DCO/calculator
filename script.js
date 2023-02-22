f50fac08a5990373c28437a8864f30bfe67c10df
S1DCO <symoun_ong@dlsu.edu.ph>
2023-02-22 14:00:01 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
+
+function operate() {}
