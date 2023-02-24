570cbe0d4e3686ada1537248aca6839119d43560
S1DCO <symoun_ong@dlsu.edu.ph>
2023-02-25 00:15:58 +0800

    added something

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 "use strict";
 
+const buttonContainer = document.querySelector("#button-container");
+
+let firstNum = 0;
+let secondNum = 0;
+
+buttonContainer.addEventListener("click", function (e) {
+  if (!e.target.closest(".btn-operand")) return;
+});
 function operate(operand, firstNum, secondNum) {}
